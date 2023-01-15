159c4fbc2bf86e5de70e1f47e36706066aac5f9c
nepster50 <99284984+nepster50@users.noreply.github.com>
2023-01-15 19:54:30 +0300

    first commit

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+    print(f'poetry run python -m brain_games.scripts.brain_games \
+            \nWelcome to the Brain Games')
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
